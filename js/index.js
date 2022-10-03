a916cd3b2f5d0960f658640ef5010da9cf5c076f
Leo-Brambilla <leo.a.brambilla@gmail.com>
2022-10-03 17:21:44 -0300

    Atualização GoodPay

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/**
+ * Trás o array do arquivo moodulosDados.js para o index.js
+ */
+import usuarios from "./moduloDados.js"
+
+
+const btnEntrar = document.getElementById('btnEntrar')
+const alertaLogin = new bootstrap.Modal("#alertaLogin")
+
+btnEntrar.addEventListener("click", () => {
+    const inputLogin = document.getElementById("login").value
+    const inputSenha = document.getElementById("senha").value
+    
+    let usuarioValido
+
+    for(let usuario of usuarios){   
+        if (usuario.nomeUsuario == inputLogin){
+        usuarioValido = usuario
+        break
+    }
+    }
+
+if (inputSenha == usuarioValido.senha || usuarioValido != inputLogin) {
+    window.location.pathname = '/app.html'
+}else {  
+    
+    alertaLogin.show('senha incorreta')
+
+
+}
+});
---
+++ js/moduloDados.js
@@ -0,0 +1,42 @@
+/**
+ * Dentro deste array existem usuários com está estrutura
+ * id
+ * nomeUsuário
+ * Senha
+ * email
+ */
+
+const usuarios = [
+    {
+        Id: 1,
+        nomeUsuario: "josé-almir",
+        senha: "12345",
+        email: "jose.almir@soulcodeacademy.org"
+    },
+    {
+        Id: 2,
+        nomeUsuario: "renato3x",
+        senha: "12345",
+        email: "renato.pereira@gmail.com"
+    },
+    {
+        Id: 3,
+        nomeUsuario: "bruno-dias",
+        senha: "12345",
+        email: "bruno.dias@gmail.com"
+    },
+    {
+        Id: 4,
+        nomeUsuario: "leonardo-dias",
+        senha: "12345",
+        email: "leonardoKK.dias@gmail.com"
+
+    }
+  
+];
+export default usuarios
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ * 
+ * 
+ */
