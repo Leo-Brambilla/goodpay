50c3e0f8305354b4315e7bd71ecc4dd93d89d298
Leo-Brambilla <leo.a.brambilla@gmail.com>
2022-10-04 13:57:20 -0300

    Montagem do sistema de erros do login e adição ao menu #31

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
 /**
- * Trás o array do arquivo moodulosDados.js para o index.js
+ * Trouxe o array do arquivo moduloDados.js
+ * para o index.js
  */
-import usuarios from "./moduloDados.js"
+ import usuarios from "./moduloDados.js"
 
-
-const btnEntrar = document.getElementById('btnEntrar')
-const alertaLogin = new bootstrap.Modal("#alertaLogin")
-
-btnEntrar.addEventListener("click", () => {
-    const inputLogin = document.getElementById("login").value
-    const inputSenha = document.getElementById("senha").value
-    
-    let usuarioValido
-
-    for(let usuario of usuarios){   
-        if (usuario.nomeUsuario == inputLogin){
-        usuarioValido = usuario
-        break
-    }
-    }
-
-if (inputSenha == usuarioValido.senha || usuarioValido != inputLogin) {
-    window.location.pathname = '/app.html'
-}else {  
-    
-    alertaLogin.show('senha incorreta')
-
-
-}
-});
+ const btnEntrar = document.getElementById("btnEntrar")
+ const alertaLogin = new bootstrap.Modal("#alertaLogin")
+ 
+ btnEntrar.addEventListener("click", () => {
+     const inputLogin = document.getElementById("login").value
+     const inputSenha = document.getElementById("senha").value
+ 
+     let usuarioValido;
+ 
+     for(let usuario of usuarios) {
+         if(usuario.nomeUsuario == inputLogin) {
+             usuarioValido = usuario;
+             break
+         }
+     }
+ 
+     if(inputSenha == usuarioValido.senha) {
+         // redirectiona o usuário para essa página html
+         window.location.pathname = '/app.html'
+     } else {
+         alertaLogin.show()
+     }
+ });
